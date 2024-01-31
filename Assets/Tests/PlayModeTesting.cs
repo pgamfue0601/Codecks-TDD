4333999f2857eadac7d717348acda45b7addfb0d
pgamfue0601 <pablo.gamiz.fuentes@hotmail.com>
2024-01-31 12:13:28 +0100

    PlayTest1 $playtest1

---
+++ Assets/Tests/PlayModeTesting.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEditor.SceneManagement;
+
+public class PlayModeTesting
+{
+    private GameObject esfera;
+    private GameObject cubo;
+
+    [SetUp]
+    public void SetUp()
+    {
+        EditorSceneManager.LoadScene("TestScene");
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator PlayModeTestingWithEnumeratorPasses()
+    {
+        yield return new WaitForSeconds(1.5f);
+        esfera = GameObject.Find("Sphere");
+        cubo = GameObject.Find("Cube");
+
+        Assert.That(cubo.transform.position.y > cubo.transform.position.y);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        EditorSceneManager.UnloadSceneAsync(EditorSceneManager.GetActiveScene());
+    }
+}
